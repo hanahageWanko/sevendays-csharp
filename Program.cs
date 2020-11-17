3e244db78f99d3036796d08bf2754f23abdfacf2
nakagawa <237s10mimi@gmail.com>
2020-11-17 22:23:19 +0900

    foreach methods

--- Program.cs
+++ Program.cs
@@ -7,17 +7,11 @@ namespace sevendays_csharp
     static void Main(string[] args)
     {
       int[] n = { 5, 4, 3, 2, 1 };
-      string[] s = { "ABC", "DEF", "GHI" };
-      int i;
-      for (i = 0; i < n.Length; i++)
+      foreach (int i in n)
       {
-        Console.Write(n[i] + " ");
+        Console.Write("{0} ", i);
       }
-      Console.WriteLine();
-      for (i = 0; i < s.Length; i++)
-      {
-        Console.Write(s[i] + " ");
-      }
+
     }
   }
 }
