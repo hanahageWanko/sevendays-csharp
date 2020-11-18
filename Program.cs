b290857d470b643cdde752919350af345eda6917
nakagawa <237s10mimi@gmail.com>
2020-11-19 03:02:06 +0900

    static member

--- Program.cs
+++ Program.cs
@@ -4,16 +4,20 @@ namespace sevendays_csharp
 {
   class Program
   {
-    //多次元連想配列
-    static void Main(string[] args)
-    {
-      //  Dataクラスを3つ作る
-      Data [] d = new Data[2];
-      Data.ShowNumber();
-      for (int i = 0; i < d.Length; i++) {
-        d[i] = new Data(i * 100);
-        Data.ShowNumber();
-      }
+    private static int snum = 100;
+    public int inum = 200;
+    public static void foo(){
+      Console.WriteLine("fooメソッド(staticメソッド)");
+    }
+    public void bar() {
+      Console.WriteLine("barメソッド(インスタンスメソッド)");
+    }
+    static void Main(string[] args) {
+      Program p = new Program();
+      Console.WriteLine("pのインスタンスフィールド:inum = {0}", p.inum);
+      Console.WriteLine("programのクラスフィールド:snum = {0}", snum);
+      foo();
+      p.bar();
     }
   }
 }
