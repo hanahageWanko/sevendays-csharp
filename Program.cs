57dde283cb67b3425def400309f479bdda6e5935
nakagawa <237s10mimi@gmail.com>
2020-11-19 17:03:24 +0900

    HashSSet

--- Program.cs
+++ Program.cs
@@ -7,14 +7,13 @@ namespace sevendays_csharp
   {
     static void Main(string[] args)
     {
-      Dictionary<String, String> capital = new Dictionary<String, String>();
-      capital["日本"] = "東京";
-      capital["イギリス"] = "ロンドン";
-      capital["フランス"]  = "パリ";
-      capital["中国"]  = "北京";
-      Console.WriteLine("世界の首都");
-      foreach(String s in capital.Keys){
-        Console.WriteLine("{0}の首都は{1}です", s, capital[s]);
+      HashSet<int> t = new HashSet<int>();
+      t.Add(1);
+      t.Add(2);
+      t.Add(3);
+      t.Add(1);
+      foreach(int i in t){
+        Console.WriteLine("{0}", i);
       }
     }
   }
