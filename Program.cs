440054ef58a68832b0de162b272006c7b50dba17
nakagawa <237s10mimi@gmail.com>
2020-11-25 00:20:59 +0900

    branch practice 1

--- Program.cs
+++ Program.cs
@@ -9,18 +9,37 @@ namespace sevendays_csharp
   {
     static void Main(string[] args)
     {
-      int i1 = 10;
-      int i2 = 4;
-      short s1 = 10;
-      short s2 = 4;
-      long l1 = 10;
-      long l2 = 4;
-      Console.WriteLine(i1 / i2);
-      Console.WriteLine(s1 / s2);
-      Console.WriteLine(l1 / l2);
-      Console.WriteLine((double)i1 / (double)i2);
-      Console.WriteLine((double)s1 / (double)s2);
-      Console.WriteLine((double)l1 / (double)l2);
+      Console.WriteLine("身長");
+      double height = double.Parse(Console.ReadLine()) * 0.01;
+      Console.WriteLine("体重");
+      double weight = double.Parse(Console.ReadLine());
+
+      double BMI = weight / (height * height);
+      Console.WriteLine("体重：{0}", weight);
+      Console.WriteLine("身長：{0}", height);
+      Console.WriteLine("BMI：{0}", BMI);
+      string result = "" ?? "測定不能";
+      if (BMI < 19.8)
+      {
+        result = "やせ型";
+      }
+      else if (19.8 <= BMI && BMI < 24.2)
+      {
+        result = "普通";
+      }
+      else if (24.2 <= BMI && BMI < 26.4)
+      {
+        result = "やや肥満（過体重)";
+      }
+      else if (26.4 <= BMI && BMI < 35.0)
+      {
+        result = "肥満";
+      }
+      else
+      {
+        result = "肥満";
+      }
+      Console.WriteLine("結果：" + result);
     }
   }
 }
