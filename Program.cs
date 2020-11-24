90ed2a3703908e79b33c3039528a37119ccb6dad
nakagawa <237s10mimi@gmail.com>
2020-11-24 23:43:21 +0900

    ufcpp practice

--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Text;
 
 namespace sevendays_csharp
 {
+
+
   class Program
   {
     static void Main(string[] args)
     {
-      HashSet<int> t = new HashSet<int>();
-      t.Add(1);
-      t.Add(2);
-      t.Add(3);
-      t.Add(1);
-      foreach(int i in t){
-        Console.WriteLine("{0}", i);
-      }
+      int i1 = 10;
+      int i2 = 4;
+      short s1 = 10;
+      short s2 = 4;
+      long l1 = 10;
+      long l2 = 4;
+      Console.WriteLine(i1 / i2);
+      Console.WriteLine(s1 / s2);
+      Console.WriteLine(l1 / l2);
+      Console.WriteLine((double)i1 / (double)i2);
+      Console.WriteLine((double)s1 / (double)s2);
+      Console.WriteLine((double)l1 / (double)l2);
     }
   }
 }
