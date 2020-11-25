e709cd4bd4a364c0df05de9fa8fe365d6ac91935
nakagawa <237s10mimi@gmail.com>
2020-11-26 00:01:18 +0900

    arr2

--- Program.cs
+++ Program.cs
@@ -9,24 +9,28 @@ namespace sevendays_csharp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("値1");
-      double x = double.Parse(Console.ReadLine());
-      Console.WriteLine("値2");
-      double y = double.Parse(Console.ReadLine());
-      Console.WriteLine("値3");
-      double z = double.Parse(Console.ReadLine());
-      double max, min;
-      double[] arr = new double[] { x, y, z };
-      max = arr[0];
-      min = arr[0];
-      for (int i = 0; i < arr.Length; i++)
+      Console.WriteLine("配列の長さを指定してください");
+      int n = int.Parse(Console.ReadLine());
+      int[] arr = new int[n];
+      for (int i = 0; i < n; i++)
       {
-        max = max < arr[i] ? arr[i] : max;
-        min = min > arr[i] ? arr[i] : min;
+        Console.Write("要素{0}:", i);
+        int x = int.Parse(Console.ReadLine());
+        arr[i] = x;
       }
+
+      int max = arr[0];
+      int min = arr[0];
+      for (int v = 0; v < arr.Length; v++)
+      {
+        max = max < arr[v] ? arr[v] : max;
+        min = min > arr[v] ? arr[v] : min;
+      }
+
 
       Console.WriteLine("MAX：{0}", max);
       Console.WriteLine("MIN：{0}", min);
+      Console.WriteLine("AVERAGE：{0}", (max + min) / 2);
     }
   }
 }
