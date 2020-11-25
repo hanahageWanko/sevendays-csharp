d3cf0a56a538db0e1de2f4e484c694bda287da42
nakagawa <237s10mimi@gmail.com>
2020-11-25 21:57:34 +0900

    branch3

--- Program.cs
+++ Program.cs
@@ -9,37 +9,24 @@ namespace sevendays_csharp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("身長");
-      double height = double.Parse(Console.ReadLine()) * 0.01;
-      Console.WriteLine("体重");
-      double weight = double.Parse(Console.ReadLine());
+      Console.WriteLine("値1");
+      double x = double.Parse(Console.ReadLine());
+      Console.WriteLine("値2");
+      double y = double.Parse(Console.ReadLine());
+      Console.WriteLine("値3");
+      double z = double.Parse(Console.ReadLine());
+      double max, min;
+      double[] arr = new double[] { x, y, z };
+      max = arr[0];
+      min = arr[0];
+      for (int i = 0; i < arr.Length; i++)
+      {
+        max = max < arr[i] ? arr[i] : max;
+        min = min > arr[i] ? arr[i] : min;
+      }
 
-      double BMI = weight / (height * height);
-      Console.WriteLine("体重：{0}", weight);
-      Console.WriteLine("身長：{0}", height);
-      Console.WriteLine("BMI：{0}", BMI);
-      string result = "" ?? "測定不能";
-      if (BMI < 19.8)
-      {
-        result = "やせ型";
-      }
-      else if (19.8 <= BMI && BMI < 24.2)
-      {
-        result = "普通";
-      }
-      else if (24.2 <= BMI && BMI < 26.4)
-      {
-        result = "やや肥満（過体重)";
-      }
-      else if (26.4 <= BMI && BMI < 35.0)
-      {
-        result = "肥満";
-      }
-      else
-      {
-        result = "肥満";
-      }
-      Console.WriteLine("結果：" + result);
+      Console.WriteLine("MAX：{0}", max);
+      Console.WriteLine("MIN：{0}", min);
     }
   }
 }
